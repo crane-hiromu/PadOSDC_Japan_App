ab239f774ce6dc86f51baf4c3619264c9d35c0c6
crane-hiromu <h.crane.t@gmail.com>
2022-08-18 16:08:25 +0900

    update

--- App/Screens/MainApp.swift
+++ App/Screens/MainApp.swift
@@ -15,8 +15,7 @@ struct MainApp: App {
     
     var body: some Scene {
         WindowGroup {
-            PlaygroundTester.PlaygroundTesterView()
-//            router.routeToRoot()
+            router.routeToRoot()
         }
     }
 }
--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "hcrane.padosdc.japan.2022",
             teamIdentifier: "R82WJ5W3TV",
-            displayVersion: "1.2",
-            bundleVersion: "6",
+            displayVersion: "1.2.1",
+            bundleVersion: "7",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [
