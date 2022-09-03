5096dfac9ae4a873a4cba7895ff81a1966347c03
crane-hiromu <h.crane.t@gmail.com>
2022-09-03 22:29:52 +0900

    append mages drag and pinch

--- App/Screens/Map/MapView.swift
+++ App/Screens/Map/MapView.swift
@@ -2,19 +2,40 @@ import SwiftUI
 
 // MARK: - View
 struct MapView: View {
+    @ObservedObject var viewModel: MapViewModel
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Image("map_university")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Image("map_floor")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            }
-            .frame(maxWidth: 672, alignment: .center)
+        VStack {
+            Asset.mapUniversity.swiftUIImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Asset.mapFloor.swiftUIImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
         }
-        .navigationBarTitle(Text("Info_Type_Map"), displayMode: .inline)
+        .frame(maxWidth: 672, alignment: .center) // iPad readable width
+        .offset(viewModel.binding.offset)
+        .scaleEffect(viewModel.binding.scale)
+        .gesture(simultaneousGesture)
+        .navigationBarTitle(L10n.infoTypeMap, displayMode: .inline)
+    }
+}
+
+// MARK: - Private
+private extension MapView {
+    
+    var simultaneousGesture: some Gesture {
+        let magnificationGesture = MagnificationGesture()
+            .onChanged { viewModel.input.didChangeImageMagnification.send($0) }
+            .onEnded { viewModel.input.didEndImageMagnification.send($0) }
+        
+        let dragGesture = DragGesture()
+            .onChanged { viewModel.input.didChangeImageOffset.send($0.translation) }
+            .onEnded{ _ in
+                viewModel.binding.initialOffset = viewModel.binding.offset
+                
+            }
+        
+        return SimultaneousGesture(magnificationGesture, dragGesture)
     }
 }
