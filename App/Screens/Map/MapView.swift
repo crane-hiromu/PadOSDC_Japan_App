a864cfaa922047a8488d8a07b1d1e4c9bf76a38f
crane-hiromu <h.crane.t@gmail.com>
2022-09-03 22:33:44 +0900

    refactor code

--- App/Screens/Map/MapView.swift
+++ App/Screens/Map/MapView.swift
@@ -16,7 +16,7 @@ struct MapView: View {
         .frame(maxWidth: 672, alignment: .center) // iPad readable width
         .offset(viewModel.binding.offset)
         .scaleEffect(viewModel.binding.scale)
-        .gesture(simultaneousGesture)
+        .gesture(SimultaneousGesture(magnificationGesture, dragGesture))
         .navigationBarTitle(L10n.infoTypeMap, displayMode: .inline)
     }
 }
@@ -24,18 +24,15 @@ struct MapView: View {
 // MARK: - Private
 private extension MapView {
     
-    var simultaneousGesture: some Gesture {
-        let magnificationGesture = MagnificationGesture()
+    var magnificationGesture: MagnificationGesture {
+        MagnificationGesture()
             .onChanged { viewModel.input.didChangeImageMagnification.send($0) }
             .onEnded { viewModel.input.didEndImageMagnification.send($0) }
-        
-        let dragGesture = DragGesture()
+    }
+    
+    var dragGesture: DragGesture {
+        DragGesture()
             .onChanged { viewModel.input.didChangeImageOffset.send($0.translation) }
-            .onEnded{ _ in
-                viewModel.binding.initialOffset = viewModel.binding.offset
-                
-            }
-        
-        return SimultaneousGesture(magnificationGesture, dragGesture)
+            .onEnded{ viewModel.input.didEndImageOffset.send($0.translation) }
     }
 }
