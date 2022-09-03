625b0ae4f8fa645546e0ec3a4ee59b46a0c7c3a3
crane-hiromu <h.crane.t@gmail.com>
2022-09-03 19:47:25 +0900

    ignore license

--- App/Screens/License/LicenseView.swift
+++ App/Screens/License/LicenseView.swift
@@ -4,27 +4,7 @@ import SwiftUI
 struct LicenseView: View {
 
     var body: some View {
-        Text("-----")
-            .onAppear {
-                let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-                do {
-                    let contentUrls = try FileManager.default.contentsOfDirectory(at: documentDirectoryURL, includingPropertiesForKeys: nil)
-                    let files = contentUrls.map{$0.lastPathComponent}
-                    print("------1 1", files)
-                } catch {
-                    print("------1 2", error)
-                }
-                
-                NSSearchPathForDirectoriesInDomains(.documentDirectory,Foundation.FileManager.SearchPathDomainMask.userDomainMask, true).forEach {
-                    do {
-                        print("------2 1", try FileManager.default.contentsOfDirectory(atPath: "\($0)"))
-                    } catch {
-                        print("------2 2", error)
-                    }
-                }
-                
-                print("-----3", Bundle.main.path(forResource: "App/Resources/License/com.mono0926.LicensePlist", ofType: "plist"))
-            }
+        Text("LicenseView")
     }
 }
 
