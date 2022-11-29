220f814412b60b1ae17371463fee6297e0d98ac7
H-Yuuki <yuukirinnma@gmail.com>
2022-11-30 00:00:02 +0900

    add: Youtubeボタンを押した時のテスト

--- AppTest/Screens/SessionDetail/SessionDetailViewModelTest.swift
+++ AppTest/Screens/SessionDetail/SessionDetailViewModelTest.swift
@@ -31,6 +31,25 @@ final class SessionDetailViewModelTest: TestCase {
         AssertExpectations([expectation], timeout: 1)
     }
     
+    func testDidTapYoutube() {
+        var cancellables: Set<AnyCancellable> = []
+        let expectation = Expectation(name: "Wait for didTapYoutube")
+        let user = SessionUser(icon: nil, name: "", twAccount: "hcrane14")
+        let model = SessionModel(track: .a, title: "", user: user, archiveParameter: "6nWnQVRVcs0")
+        let viewModel = SessionDetailViewModel(output: .init(model: model))
+        
+        viewModel.output.openSns
+            .sink { url in
+                AssertEqual(url.absoluteString, other: "https://www.youtube.com/watch?v=6nWnQVRVcs0")
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        viewModel.input.didTapYoutube.send(())
+        
+        AssertExpectations([expectation], timeout: 1)
+    }
+    
     func testDidTapClose() {
         var cancellables: Set<AnyCancellable> = []
         let expectation = Expectation(name: "Wait for didTapSns")
